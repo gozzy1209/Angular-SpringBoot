e071d6adb4cd96b844f0c5d7766afb8fda15437d
Zhaoyan Zhang <gozzy1209@foxmail.com>
2022-12-18 19:12:57 -0800

    Angular SpringBoot

---
+++ springboot-backend/src/test/java/EmployeeManagementSystem/ApplicationTests.java
@@ -0,0 +1,14 @@
+package EmployeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//1111
+	}
+
+}
